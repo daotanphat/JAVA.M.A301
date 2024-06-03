cd51e4bb1eb0e33856596aaf6e808dcb7cf953eb
Dao Tan Phat <daotanphat140703@gmail.com>
2024-06-03 14:45:55 +0700

    feat: add menu for program & handle choice add persons to list

--- src/fa/training/main/PersonManage.java
+++ src/fa/training/main/PersonManage.java
@@ -1,7 +1,35 @@
 package fa.training.main;
 
+import fa.training.entities.Person;
+import fa.training.service.PersonService;
+import fa.training.utils.Constants;
+import fa.training.utils.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonManage {
     public static void main(String[] args) {
+        Constants constants = new Constants();
+        Validator validator = new Validator();
+        PersonService personService = new PersonService();
+        List<Person> personList = new ArrayList<>();
 
+        int choice;
+        do {
+            constants.menu();
+            choice = validator.inputInt("Enter your choice", 1, 4);
+            switch (choice) {
+                case 1:
+                    personList = personService.addPersons();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }while (validator.checkQuit(choice));
     }
 }
