6d5a4bb66e4f00d22af216a3e655eeca546fb954
Dao Tan Phat <daotanphat140703@gmail.com>
2024-06-03 13:32:29 +0700

    feat: add object Teacher

---
+++ src/fa/training/entities/Teacher.java
@@ -0,0 +1,37 @@
+package fa.training.entities;
+
+public class Teacher extends Person {
+    private double basicSalary;
+    private double subsidy;
+
+    public Teacher(String name, String gender, String phoneNumber, String email, double basicSalary, double subsidy) {
+        super(name, gender, phoneNumber, email);
+        this.basicSalary = basicSalary;
+        this.subsidy = subsidy;
+    }
+
+    public double getBasicSalary() {
+        return basicSalary;
+    }
+
+    public void setBasicSalary(double basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    public double getSubsidy() {
+        return subsidy;
+    }
+
+    public void setSubsidy(double subsidy) {
+        this.subsidy = subsidy;
+    }
+
+    public double calculateSalary() {
+        return basicSalary + subsidy;
+    }
+
+    @Override
+    void purchaseParkingPass() {
+
+    }
+}
