427c0cf461fe9a2efe2871d91aac4c6e5e84fae0
Dao Tan Phat <daotanphat140703@gmail.com>
2024-06-03 14:46:33 +0700

    feat: add display menu for program

---
+++ src/fa/training/utils/Constants.java
@@ -0,0 +1,12 @@
+package fa.training.utils;
+
+public class Constants {
+    public void menu(){
+        System.out.println("------Person Manage------");
+        System.out.println("1. Add Person\n" +
+                "2. Update Person\n" +
+                "3. Display teacher(who has a salary > 1000$)\n" +
+                "4. Report(All students qualify pass the course: final mark > 6)\n" +
+                "Other to quit...");
+    }
+}
